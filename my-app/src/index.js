67b21c55c2e51c854b238887430d512545c8a44d
NguyenDuongViHao <146154160+NguyenDuongViHao@users.noreply.github.com>
2024-06-25 00:56:25 +0700

    delete myapp

--- my-app/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Router from './Router/Router';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Router />);
