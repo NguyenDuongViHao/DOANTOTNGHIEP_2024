0f93c24923a2268f343ee5e4a0d16fcfdcb7e921
NguyenDuongViHao <146154160+NguyenDuongViHao@users.noreply.github.com>
2024-07-04 10:31:04 +0700

    invoice view model

---
+++ Project_ClothingStore/ClothingStore/Models/InvoiceViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace ClothingStore.Models
+{
+	public class InvoiceViewModel
+	{
+		public int Id { get; set; }
+
+		public string UserName { get; set; }
+
+		public string IssueDate { get; set; }
+
+		public string ShippingAddress { get; set; }
+
+		public string ShippingPhone { get; set; }
+
+		public double Discount { get; set; }
+
+		public double Total { get; set; }
+
+		public int TotalQuantity { get; set; }
+
+		public string ApproveOrder { get; set; }
+
+		public bool COD { get; set; }
+
+		public bool MoMo { get; set; }
+
+		public string NameProduct { get; set; }
+
+		public bool Status { get; set; }
+
+	}
+}
